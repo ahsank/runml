a113a83b4b6b125a934378e28f193dae1e06cfbd
Ahsan Khan <ahsankhanmail@gmail.com>
2022-02-08 16:26:59 -0800

    Add minimal setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup
+
+setup(name='runml',
+      version='0.0.1',
+      description='Run Machine learning tensorflow model to predict sequence',
+      url='https://github.com/ahsank/runml',
+      author='Ahsan Khan',
+      author_email='ahsankhanmail@gmail.com',
+      license='MIT',
+      packages=['runml'],
+      zip_safe=False)
