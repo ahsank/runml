df7bffe73d283e148aac3d1327cf11918c4b9c27
Ahsan Khan <ahsankhanmail@gmail.com>
2024-04-07 16:21:37 -0500

    Increase version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 from setuptools import setup
 
 setup(name='runml',
-      version='0.0.5',
+      version='0.0.6',
       description='Run Machine learning tensorflow model to predict sequence',
       url='https://github.com/ahsank/runml',
       author='Ahsan Khan',
